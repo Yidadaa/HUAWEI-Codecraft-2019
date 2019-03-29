5a659f6c266a2b03bec34979d1509fa3529dba54
yidadaa <yidadaa@qq.com>
2019-03-29 21:17:46 +0800

    feat(test): 优化测试脚本

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 # 如果build路径不存在，则创建该路径
-if [ ! -d './build' ]; then
-  mkdir build
+if [ -d './build' ]; then
+  rm -rf build
 fi
+
+if [ -d './bin' ]; then
+  rm -rf bin
+fi
+
+mkdir build
 
 cd build
 cmake ..

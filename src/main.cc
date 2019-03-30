b9dd2e1234843b918a460d5b94df1d64eb3f3af3
Yidadaa <yidadaa@qq.com>
2019-03-30 17:19:48 +0800

    fix(main): fix typo

--- src/main.cc
+++ src/main.cc
@@ -2,12 +2,12 @@
 #include "core/basic_types.h"
 
 int main(int argc, char const *argv[]) {
-	if(argc < 2){
-		std::cout << "please input args: carPath, roadPath, crossPath, answerPath" << std::endl;
-		exit(1);
-	}
-	
-	std::string car_path(argv[1]);
+  if(argc < 2){
+    std::cout << "please input args: carPath, roadPath, crossPath, answerPath" << std::endl;
+    exit(1);
+  }
+
+  std::string car_path(argv[1]);
   std::string road_path(argv[2]);
   std::string cross_path(argv[3]);
   std::string answer_path(argv[4]);
